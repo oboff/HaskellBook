8b95227a8cedac288f96eb8e7b29e64fa9ba3eaf
Orlando <orlando.boffill@gmx.com>
2015-11-09 00:55:06 +1100

    commit

--- Ch5.9.Ex.hs
+++ Ch5.9.Ex.hs
@@ -27,3 +27,23 @@
 --
 -- 1. functionH :: [t] -> t
 -- 2. functionC :: Ord a => a -> a -> Bool
+-- 3. functionS :: (a, b) -> b
+--
+-- 1. 
+i :: a -> a
+i x = x
+-- 2. 
+c :: a -> b -> a
+c x y = x
+-- 3. 
+c'' :: b -> a -> b
+c'' x y = x
+-- 4. 
+c' :: a -> b -> b
+c' x y = y
+-- 5.
+r :: [a] -> [a]
+r x = reverse x
+-- 6.
+co :: (b -> c) -> (a -> b) -> (a -> c)
+co x y z = x $ y z
